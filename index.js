bf81815c2dae1710c0a7f21889ac0aa91f3874b4
Johnson <sakajohnson1@gmail.com>
2025-05-05 13:57:48 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const shareButton = document.querySelector(".share-icon");
+
+const shareIcons = document.querySelector(".tooltip");
+
+shareButton.addEventListener("click" , () => {
+  shareIcons.style.display = "flex";
+  
+});
